d0132837011cdcacb2b331345d65e75021fcef96
ksenzo <ksenz.gleb61@gmail.com>
2022-01-21 19:23:07 +0200

    create and adaptive second section

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,7 +2,9 @@ let fixedHeader = document.querySelector('.header');
 let openMenuBurger = document.querySelector('.header__btn-mobile');
 let menuMobile = document.querySelector('.menu-mobile');
 let openMenuBurgerSpan = document.querySelector('.header__span');
-
+let downBtn = document.querySelectorAll('.services__down-vector');
+let addText = document.querySelectorAll('.services__service-addtext');
+let serviceStatus = document.querySelectorAll('.services__status');
 
 window.onscroll = function showHeader() {
    let fixedHeader = document.querySelector('.header');
@@ -20,3 +22,12 @@ function menuBurger() {
 }
 
 openMenuBurger.addEventListener('click', () => menuBurger());
+
+
+for (let i = 0; i < downBtn.length; i++) {
+   downBtn[i].addEventListener("click", function() {
+      downBtn[i].classList.toggle("__active");
+      addText[i].classList.toggle("__active");
+      serviceStatus[i].classList.toggle("__active");
+   });
+}
